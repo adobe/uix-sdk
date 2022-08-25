d8ffb05293c569ee819b13630e77ba7ad4330b34
Volodymyr Kublytskyi <kublytsk@adobe.com>
2022-08-25 16:09:57 -0500

    Move Extension Registry code after merge

--- packages/uix-host/src/extensions-provider/extension-registry.ts
+++ packages/uix-host/src/extensions-provider/extension-registry.ts
@@ -1,16 +1,54 @@
 import { InstalledExtensions, ExtensionsProvider } from "../host.js";
 
+interface ExtensionDefinition {
+  name: string;
+  title: string;
+  description: string;
+  icon: string;
+  publisher: string;
+  endpoints: Record<string, EndpointDefinition>;
+  xrInfo: ExtensionInfo;
+  status: string;
+}
+
+type EndpointDefinition = Record<string, Array<OperationDefinition>>;
+
+interface ExtensionInfo {
+  supportEmail: string;
+  appId: string;
+}
+
+interface OperationDefinition {
+  href: string;
+  metadata: OperationMetadata;
+}
+
+interface OperationMetadata {
+  services: Array<object>;
+  profile: OperationProfile;
+}
+
+interface OperationProfile {
+  client_id: string;
+  scope: string;
+}
+
 /** @public */
-export interface ExtensionRegistryRegistration {
+export interface ExtensionRegistryEndpointRegistration {
   service: string;
   extensionPoint: string;
   version: string;
+}
+
+/** @public */
+export interface ExtensionRegistryExtensionRegistration
+  extends ExtensionRegistryEndpointRegistration {
   imsOrg: string;
 }
 
 /** @public */
 export interface ExtensionRegistryConnection {
-  url: string;
+  host?: string;
   apiKey: string;
   auth: {
     schema: "Basic" | "Bearer";
@@ -19,13 +57,59 @@ export interface ExtensionRegistryConnection {
 }
 
 export interface ExtensionRegistryConfig
-  extends ExtensionRegistryRegistration,
+  extends ExtensionRegistryExtensionRegistration,
     ExtensionRegistryConnection {}
+
+function buildEndpointPath(
+  config: ExtensionRegistryEndpointRegistration
+): string {
+  return `${config.service}/${config.service}/${config.version}`;
+}
+
+async function fetchExtensionsFromRegistry(
+  config: ExtensionRegistryConfig
+): Promise<Array<ExtensionDefinition>> {
+  const resp = await fetch(
+    `https://${
+      config.host || "appregistry.adobe.io"
+    }/myxchng/v1/org/${encodeURIComponent(
+      config.imsOrg
+    )}/xtn/${buildEndpointPath(config)}?auth=true`,
+    {
+      headers: {
+        Accept: "application/json",
+        Authorization: `${config.auth.schema} ${config.auth.imsToken}`, // todo: check if auth schema needed (initial implementation was without it)
+        "X-Api-Key": config.apiKey,
+      },
+    }
+  );
+
+  if (resp.status != 200) {
+    throw new Error(
+      `extension registry returned non-200 response (${
+        resp.status
+      }): ${await resp.text()}`
+    );
+  }
+
+  return await resp.json();
+}
 
 function extensionRegistryExtensionsProvider(
   config: ExtensionRegistryConfig
 ): Promise<InstalledExtensions> {
-  // todo: add implementation here
+  const erEndpoint = buildEndpointPath(config);
+  return fetchExtensionsFromRegistry(config).then((out) =>
+    out.reduce(
+      (a, e) => ({
+        ...a,
+        // todo: make safer way to extract href
+        [e.name]: e.endpoints[erEndpoint].view[0].href,
+      }),
+      {}
+    )
+  );
+
   return Promise.resolve({});
 }
 
--- packages/uix-host/src/index.ts
+++ packages/uix-host/src/index.ts
@@ -1,5 +1,3 @@
 export * from "./host.js";
-export * from "./registry.js";
-export * from "./extension-loader.js";
 export type { PortOptions } from "./port.js";
 export * from "./extensions-provider/index.js";
