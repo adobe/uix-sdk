ec429eaef0491529559068ba1795d899e545e77a
Volodymyr Kublytskyi <kublytsk@adobe.com>
2022-08-25 21:20:49 -0500

    Extensions providers composition should return extensions provider but not providers

--- packages/uix-host/src/extensions-provider/composition.ts
+++ packages/uix-host/src/extensions-provider/composition.ts
@@ -2,10 +2,11 @@ import { InstalledExtensions, ExtensionsProvider } from "../host.js";
 
 export function combineExtensionsFromProviders(
   ...providers: Array<ExtensionsProvider>
-): Promise<InstalledExtensions> {
-  return Promise.all(providers.map((ep: ExtensionsProvider) => ep())).then(
-    (extensionsBatches: Array<InstalledExtensions>) => {
-      return Object.assign({}, ...extensionsBatches);
-    }
-  );
+): ExtensionsProvider {
+  return () =>
+    Promise.all(providers.map((ep: ExtensionsProvider) => ep())).then(
+      (extensionsBatches: Array<InstalledExtensions>) => {
+        return Object.assign({}, ...extensionsBatches);
+      }
+    );
 }
