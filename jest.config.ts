b23169e98a8aff4806c0506f401bc4f86fc3e481
jzetlen <jzetlen@adobe.com>
2023-01-23 12:27:02 -0600

    add tests for normalizers

--- jest.config.ts
+++ jest.config.ts
@@ -1,32 +1,37 @@
 import type { JestConfigWithTsJest } from "ts-jest";
 
-const jestConfig: JestConfigWithTsJest = {
+const sdkProject = (sdkName: string, overrides: JestConfigWithTsJest) => ({
+  displayName: `uix-${sdkName}`,
+  testMatch: [`<rootDir>/packages/uix-${sdkName}/src/**/*.test.ts`],
+  modulePathIgnorePatterns: ["<rootDir>/dist"],
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  globals: {
+    UIX_SDK_VERSION: "0.0.1-test",
+    UIX_SDK_BUILDMODE: "test",
+  },
+  ...overrides,
+});
+
+const jestConfig = {
   extensionsToTreatAsEsm: [".ts"],
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
   projects: [
-    {
-      displayName: "uix-core",
-      testMatch: ["<rootDir>/packages/uix-core/src/**/*.test.ts"],
-      modulePathIgnorePatterns: ["<rootDir>/dist"],
+    sdkProject("core", {
       setupFiles: [
         "<rootDir>/packages/uix-core/src/__helpers__/jest.messagechannel.cjs",
       ],
-      testEnvironment: "jsdom",
-      transform: {
-        "^.+\\.tsx?$": [
-          "ts-jest",
-          {
-            useESM: true,
-          },
-        ],
-      },
-      globals: {
-        UIX_SDK_VERSION: "0.0.1-test",
-        UIX_SDK_BUILDMODE: "test",
-      },
-    },
+    }),
+    sdkProject("host", {}),
   ],
 };
 
