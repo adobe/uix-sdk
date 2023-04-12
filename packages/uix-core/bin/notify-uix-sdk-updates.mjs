5509041995bebcc03f83fc5d7f7e8ae3a0770d25
jzetlen <jzetlen@adobe.com>
2023-04-12 12:14:45 -0500

    wip update-notifier and reorg of stuff for CLI packages

--- packages/uix-core/bin/notify-uix-sdk-updates.mjs
+++ packages/uix-core/bin/notify-uix-sdk-updates.mjs
@@ -1,4 +1,24 @@
+import path from "path";
 import { readFile } from "fs/promises";
 import updateNotifier from "update-notifier";
 
-async function main() {}
+const [name, version] = process.argv.slice(2);
+
+async function main() {
+  updateNotifier({
+    shouldNotifyInNpmScript: true,
+    updateCheckInterval: 30,
+    pkg: {
+      name,
+      version,
+    },
+  }).notify({
+    message:
+      "The {packageName} is out of date. Update it to {latestVersion} asap!!",
+  });
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
