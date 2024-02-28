3ceb5ad3b01b5d16c70d13147a947b19568aeb10
Nitin Jadhav <3931042+nitinja@users.noreply.github.com>
2024-02-28 09:56:34 -0600

    Added test case

--- packages/uix-host/src/extensions-provider/extension-registry.test.ts
+++ packages/uix-host/src/extensions-provider/extension-registry.test.ts
@@ -1,4 +1,5 @@
-import { enableFetchMocks } from "jest-fetch-mock";
+import fetch, { enableFetchMocks } from "jest-fetch-mock";
+import fetchMock from "jest-fetch-mock";
 
 const {
   createExtensionRegistryAsObjectsProvider,
@@ -6,21 +7,71 @@ const {
 
 enableFetchMocks();
 
-window.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve({ rates: { CAD: 1.42 } }),
-  })
-);
-
 describe("Extension Registry", () => {
-  beforeAll(() => jest.spyOn(window, "fetch"));
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
 
-  test('[3] should result in "fizz"', () => {
-    window.fetch.mockResolvedValueOnce({
-      ok: true,
-      json: async () => ({ success: true }),
+  test("createExtensionRegistryAsObjectsProvider()() should return published extension objects", async () => {
+    fetch.mockResponseOnce(
+      JSON.stringify([
+        {
+          name: "my-test-extn",
+          endpoints: {
+            "myservice/testextpoint/1": {
+              view: [
+                {
+                  href: "https://example.com/index.html",
+                },
+              ],
+            },
+          },
+          status: "PUBLISHED",
+        },
+        {
+          name: "my-test-extn-1",
+          endpoints: {
+            "myservice/testextpoint/1": {
+              view: [
+                {
+                  href: "https://example.com/index.html",
+                },
+              ],
+            },
+          },
+          status: "DRAFT",
+        },
+      ])
+    );
+
+    const config = {
+      service: "myservice",
+      extensionPoint: "testextpoint",
+      version: "1",
+      imsOrg: "test-org",
+      baseUrl: "http://example.com",
+      apiKey: "test-key",
+      auth: {
+        schema: "Basic",
+        imsToken: "test-token",
+      },
+    };
+    const extensionRegistry = createExtensionRegistryAsObjectsProvider(config);
+    const extensions = await extensionRegistry();
+
+    expect(extensions).toEqual({
+      "my-test-extn": {
+        id: "my-test-extn",
+        url: "https://example.com/index.html",
+        extensionPoints: ["myservice/testextpoint/1"],
+      },
     });
-
-    expect(1).toBe(1);
+    expect(extensions).not.toContain({
+      "my-test-extn-1": {
+        id: "my-test-extn-1",
+        url: "https://example.com/index.html",
+        extensionPoints: ["myservice/testextpoint/1"],
+      },
+    });
   });
 });
