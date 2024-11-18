4ee4a36a7bb2f3e9621f45a050fb05bc65cc311e
Amy Zhao <azhao@adobe.com>
2024-11-18 11:27:25 -0600

    build

--- packages/uix-host-react/src/components/Extensible.tsx
+++ packages/uix-host-react/src/components/Extensible.tsx
@@ -20,7 +20,7 @@ import type {
   SharedContextValues,
 } from "@adobe/uix-host";
 import { Host } from "@adobe/uix-host";
-import { ExtensionContext } from "../extension-context.ts";
+import { ExtensionContext } from "../extension-context.js";
 
 /** @public */
 export interface ExtensibleProps extends Omit<HostConfig, "hostName"> {
