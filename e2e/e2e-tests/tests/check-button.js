65451ad121e96b01d370f1ae7f31187de4dc998c
Łukasz Molski <lmolski@sii.pl>
2026-01-20 14:56:14 +0100

    Update format

--- e2e/e2e-tests/tests/check-button.js
+++ e2e/e2e-tests/tests/check-button.js
@@ -1,37 +1,40 @@
-import { fixture, test, Selector } from 'testcafe';
+import { fixture, test, Selector } from "testcafe";
 
-const iframeSelector = '#iframe-for-guest';
+const iframeSelector = "#iframe-for-guest";
 
-fixture('UIX Host App')
-    .page('http://localhost:3000');
+fixture("UIX Host App").page("http://localhost:3000");
 
-test('Check if guest loaded', async t => {
-    const iframe = Selector(iframeSelector);
-    await t.expect(iframe.exists).ok('Iframe should exist');
+test("Check if guest loaded", async (t) => {
+  const iframe = Selector(iframeSelector);
+  await t.expect(iframe.exists).ok("Iframe should exist");
 
-    const iframeSrc = await iframe.getAttribute('src');
-    console.log('Iframe src:', iframeSrc);
-    await t.expect(iframe.getAttribute('src')).notEql('', 'Iframe should have src attribute');
+  const iframeSrc = await iframe.getAttribute("src");
+  console.log("Iframe src:", iframeSrc);
+  await t
+    .expect(iframe.getAttribute("src"))
+    .notEql("", "Iframe should have src attribute");
 });
 
-test('Check response from guest app', async t => {
-    const guestMessageButton = Selector('#get-guest-message-button');
-    await t.click(guestMessageButton);
+test("Check response from guest app", async (t) => {
+  const guestMessageButton = Selector("#get-guest-message-button");
+  await t.click(guestMessageButton);
 
-    const guestMessage = Selector('#get-guest-message-result').innerText;
-    await t.expect(guestMessage).ok();
+  const guestMessage = Selector("#get-guest-message-result").innerText;
+  await t.expect(guestMessage).ok();
 });
 
-test('Set message from host', async t => {
-    await t.click('#set-message-from-host');
-    
-    const iframe = Selector(iframeSelector);
-    await t.expect(iframe.exists).ok('Iframe should exist');
+test("Set message from host", async (t) => {
+  await t.click("#set-message-from-host");
 
-    await t.expect(iframe.getAttribute('src')).notEql('', 'Iframe should have src attribute');
+  const iframe = Selector(iframeSelector);
+  await t.expect(iframe.exists).ok("Iframe should exist");
 
-    await t.switchToIframe(iframeSelector);
-        
-    const textParagraph = Selector('#text-from-host').exists;
-    await t.expect(textParagraph).ok('Text from host should exist in iframe');
+  await t
+    .expect(iframe.getAttribute("src"))
+    .notEql("", "Iframe should have src attribute");
+
+  await t.switchToIframe(iframeSelector);
+
+  const textParagraph = Selector("#text-from-host").exists;
+  await t.expect(textParagraph).ok("Text from host should exist in iframe");
 });
