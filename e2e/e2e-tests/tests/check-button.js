2e1d704a25b1a33a7b4643aa6e6b42fe3cfd2d39
Łukasz Molski <lmolski@sii.pl>
2026-01-23 12:04:48 +0100

    update e2e

--- e2e/e2e-tests/tests/check-button.js
+++ e2e/e2e-tests/tests/check-button.js
@@ -5,39 +5,35 @@ const iframeSelector = "#iframe-for-guest";
 fixture("UIX Host App").page("http://localhost:3000");
 
 test("Check if guest is loaded", async (t) => {
-  t.wait(5000);
   const iframe = Selector(iframeSelector);
-  await t.expect(iframe.exists).ok("Iframe should exist");
+  await t.expect(iframe.exists).ok("Iframe should exist", { timeout: 10000 });
 
   const iframeSrc = await iframe.getAttribute("src");
-  console.log("Iframe src:", iframeSrc);
   await t
     .expect(iframe.getAttribute("src"))
-    .notEql("", "Iframe should have src attribute");
+    .notEql("", "Iframe should have src attribute", { timeout: 10000 });
 });
 
 test("Check response from guest app", async (t) => {
-  t.wait(5000);
   const guestMessageButton = Selector("#get-guest-message-button");
   await t.click(guestMessageButton);
 
   const guestMessage = Selector("#get-guest-message-result").innerText;
-  await t.expect(guestMessage).ok();
+  await t.expect(guestMessage).ok("Guest message should be displayed", { timeout: 10000 });
 });
 
 test("Set message from host", async (t) => {
-  t.wait(5000);
   await t.click("#set-message-from-host");
 
   const iframe = Selector(iframeSelector);
-  await t.expect(iframe.exists).ok("Iframe should exist");
+  await t.expect(iframe.exists).ok("Iframe should exist", { timeout: 10000 });
 
   await t
     .expect(iframe.getAttribute("src"))
-    .notEql("", "Iframe should have src attribute");
+    .notEql("", "Iframe should have src attribute", { timeout: 10000 });
 
   await t.switchToIframe(iframeSelector);
 
   const textParagraph = Selector("#text-from-host").exists;
-  await t.expect(textParagraph).ok("Text from host should exist in iframe");
+  await t.expect(textParagraph).ok("Text from host should exist in iframe", { timeout: 10000 });
 });
