9a9ebc1e49998ca61763dc11cfa243ec3c6a4ffa
Łukasz Molski <lmolski@sii.pl>
2026-01-23 11:50:45 +0100

    update e2e

--- e2e/e2e-tests/tests/check-button.js
+++ e2e/e2e-tests/tests/check-button.js
@@ -5,6 +5,7 @@ const iframeSelector = "#iframe-for-guest";
 fixture("UIX Host App").page("http://localhost:3000");
 
 test("Check if guest is loaded", async (t) => {
+  t.wait(5000);
   const iframe = Selector(iframeSelector);
   await t.expect(iframe.exists).ok("Iframe should exist");
 
@@ -16,6 +17,7 @@ test("Check if guest is loaded", async (t) => {
 });
 
 test("Check response from guest app", async (t) => {
+  t.wait(5000);
   const guestMessageButton = Selector("#get-guest-message-button");
   await t.click(guestMessageButton);
 
@@ -24,6 +26,7 @@ test("Check response from guest app", async (t) => {
 });
 
 test("Set message from host", async (t) => {
+  t.wait(5000);
   await t.click("#set-message-from-host");
 
   const iframe = Selector(iframeSelector);
